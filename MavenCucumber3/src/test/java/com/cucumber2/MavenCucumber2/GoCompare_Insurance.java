2b52cdd03a146f7c54895934d552a4be3fd86023
mitchell <mitchell_agoma@yahoo.co.uk>
2015-09-28 23:23:50 +0100

    Update

--- MavenCucumber3/src/test/java/com/cucumber2/MavenCucumber2/GoCompare_Insurance.java
+++ MavenCucumber3/src/test/java/com/cucumber2/MavenCucumber2/GoCompare_Insurance.java
@@ -5,12 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class GoCompare_Insurance {
 	
 	
-	WebElement element;
-	WebDriver e;
+	static WebElement element;
+	static WebDriver e;
 	String text;
 	
 	
-	 public WebElement Select_Insurance_Type(String Insurance_Type){
+	 public static WebElement Select_Insurance_Type(String Insurance_Type){
 		element = e.findElement(By.xpath(Insurance_Type));
 		 return element;
 	 }
