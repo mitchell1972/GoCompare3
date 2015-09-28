036a45e4a9eff447f7dd3c29325b7bb5acb7b1d6
mitchell <mitchell_agoma@yahoo.co.uk>
2015-09-28 07:24:17 +0100

    Added class GoCompare_Insurance

---
+++ MavenCucumber3/src/test/java/com/cucumber2/MavenCucumber2/GoCompare_Insurance.java
@@ -0,0 +1,14 @@
+package com.cucumber2.MavenCucumber2;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoCompare_Insurance {
+	
+	WebElement element;
+	String text;
+	 public WebElement Select_Insurance_Type(WebElement e){
+		 element = e;
+		 return element;
+	 }
+
+}
