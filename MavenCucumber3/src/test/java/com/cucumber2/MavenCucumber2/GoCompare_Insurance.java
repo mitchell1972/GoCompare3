adffb457657d71872a45d2cb11780504e96bb8ad
mitchell <mitchell_agoma@yahoo.co.uk>
2015-09-30 23:39:37 +0100

    updated

--- MavenCucumber3/src/test/java/com/cucumber2/MavenCucumber2/GoCompare_Insurance.java
+++ MavenCucumber3/src/test/java/com/cucumber2/MavenCucumber2/GoCompare_Insurance.java
@@ -4,20 +4,20 @@ import org.openqa.selenium.*;
 
 public class GoCompare_Insurance {
 	
-	WebDriver e;
+	static WebDriver e;
 	WebElement ex;
 	String test;
 	
 	 public void Select_Insurance_Type(String Insurance_Type){
 		 try {
 			//if(Insurance_Type.contains(label)){
-				ex = e.findElement(By.xpath(Insurance_Type));
+				
 			e.findElement(By.xpath(Insurance_Type)).click();
 			
 			
 			//}
 			//else 
-				System.out.println("Can't find element on page");
+				//System.out.println("Can't find element on page");
 			System.out.println(ex.getText());
 		} catch (Exception e1) {
 			
